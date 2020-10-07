ef4cbc8081b05107be8227585aabc87dc36aed8a
Nag <Nag@LAPTOP-9SJMQIUO>
2020-10-07 15:31:45 +0530

    User Part

---
+++ ewallet-userms/src/main/java/com/capg/ewallet/repo/UserRepo.java
@@ -0,0 +1,13 @@
+package com.capg.ewallet.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.capg.ewallet.model.WalletAccount;
+import com.capg.ewallet.model.WalletUser;
+
+public interface UserRepo extends JpaRepository<WalletUser, Integer>{
+
+//	public WalletUser findByUserIdAndPassword(int userId,String password);
+
+
+}
---
+++ ewallet-userms/src/main/java/com/capg/ewallet/service/UserService.java
@@ -0,0 +1,25 @@
+package com.capg.ewallet.service;
+
+import java.util.List;
+
+import com.capg.ewallet.errors.AccountNotFoundException;
+import com.capg.ewallet.errors.UserAlreadyExistsException;
+import com.capg.ewallet.errors.UserNotFoundException;
+import com.capg.ewallet.model.WalletAccount;
+import com.capg.ewallet.model.WalletUser;
+
+public interface UserService {
+
+	public WalletUser createWalletUser(WalletUser walletUser) throws UserAlreadyExistsException;
+
+	public List<WalletUser> getAllWalletUser();
+
+	public WalletAccount getOneAccount(int accountId) throws AccountNotFoundException;
+
+//	public WalletUser getUser(int userId, String password) throws UserNotFoundException;
+
+	public void deleteWalletUser(int userId);
+	
+	
+
+}
